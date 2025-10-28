28ae97c846b84ed9dfe4eff3d8efede015f90004
khalid-dev24 <khalidmisbaudeen04@gmail.com>
2025-10-28 22:21:50 +0100

    updated file path

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new Environment($loader);
 
 $page = $_GET['page'] ?? 'home';
